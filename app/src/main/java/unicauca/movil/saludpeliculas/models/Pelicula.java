15ac6382bc8080ddc9843136ed642f449d5fa737
Dario Chamorro <dario.chamorro.v@gmail.com>
2017-04-21 11:20:21 -0500

    Template implementado

---
+++ app/src/main/java/unicauca/movil/saludpeliculas/models/Pelicula.java
@@ -0,0 +1,39 @@
+package unicauca.movil.saludpeliculas.models;
+
+public class Pelicula {
+
+    String nombre, descripcion, imagen, fechaEstreno;
+    int duracion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getFechaEstreno() {
+        return fechaEstreno;
+    }
+
+    public void setFechaEstreno(String fechaEstreno) {
+        this.fechaEstreno = fechaEstreno;
+    }
+}
